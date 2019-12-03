d19c90acd7c71b0e03cd48cd7187962141a45f4b
indresha Mn <indreshan@emdis.com>
2019-12-03 16:15:43 +0530

    first Commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,37 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld :msg="mesg" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import HelloWorld from "@/components/HelloWorld.vue";
+import http from "../http-common";
 export default {
-  name: 'home',
+  name: "home",
+  data() {
+    return { mesg:{} };
+  },
   components: {
     HelloWorld
+  },
+  mounted() {
+    this.getMessage();
+  },
+  methods: {
+    getMessage() {
+      http
+        .get(`/teams`+`/india-One`)
+        .then(response => {
+          this.mesg = response.data;
+          console.log(response.data);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    }
   }
-}
+};
 </script>
